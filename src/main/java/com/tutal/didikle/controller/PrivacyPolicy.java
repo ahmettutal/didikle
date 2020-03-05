859d7a2efd023d291b65b92fbdce6530353075bd
atutal <Dokuzz09>
2020-03-06 00:42:00 +0300

    peh

--- src/main/java/com/tutal/didikle/controller/PrivacyPolicy.java
+++ src/main/java/com/tutal/didikle/controller/PrivacyPolicy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PrivacyPolicy {
 
-    @GetMapping({"/privacy-policy"})
+    @GetMapping("/privacy-policy")
     public String hello() {
 
         return "privacy-policy";
